e727a081b1aaf94045da92dccc1c2cadfdac641d
James Arcus <jimbo@ucc.asn.au>
2020-04-21 13:58:34 +0800

    Source API token from outside driver

--- cf_driver.py
+++ cf_driver.py
@@ -19,37 +19,35 @@ def cf_patch(token, path, data):
     }
     return requests.patch(CF_BASEPATH + path, json=data, headers=headers)
 
-CF_TOKEN = os.getenv("CF_TOKEN")
-
-def get_zone_id(zone_name):
+def get_zone_id(token, zone_name):
     endpoint = "/zones"
     query = {"name": zone_name}
 
-    response = cf_get(CF_TOKEN, endpoint, query)
+    response = cf_get(token, endpoint, query)
     print(response.text)
 
-def get_zone_details(zone_id):
+def get_zone_details(token, zone_id):
     endpoint = "/zones/{}".format(zone_id)
 
-    response = cf_get(CF_TOKEN, endpoint)
+    response = cf_get(token, endpoint)
     print(response.text)
 
-def get_dns_record_id(zone_id, dns_record_name):
+def get_dns_record_id(token, zone_id, dns_record_name):
     endpoint = "/zones/{}/dns_records".format(zone_id)
     query = {"name": dns_record_name}
 
-    response = cf_get(CF_TOKEN, endpoint, query)
+    response = cf_get(token, endpoint, query)
     print(response.text)
 
-def get_dns_record_details(zone_id, dns_record_id):
+def get_dns_record_details(token, zone_id, dns_record_id):
     endpoint = "/zones/{}/dns_records/{}".format(zone_id, dns_record_id)
 
-    response = cf_get(CF_TOKEN, endpoint)
+    response = cf_get(token, endpoint)
     print(response.text)
 
-def patch_dns_record_address(zone_id, dns_record_id, address):
+def patch_dns_record_address(token, zone_id, dns_record_id, address):
     endpoint = "/zones/{}/dns_records/{}".format(zone_id, dns_record_id)
     params = {"content": address}
 
-    response = cf_patch(CF_TOKEN, endpoint, params)
+    response = cf_patch(token, endpoint, params)
     print(response.text)
